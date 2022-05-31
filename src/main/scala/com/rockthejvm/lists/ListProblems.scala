413728798f4cf967f3b1a559053bd0d0ea42194d
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-05-31 11:49:05 +0530

    Get N-th element from list

---
+++ src/main/scala/com/rockthejvm/lists/ListProblems.scala
@@ -0,0 +1,64 @@
+package com.rockthejvm.lists
+
+import scala.annotation.{tailrec, targetName}
+
+abstract class RList[+T] {
+  def head: T
+
+  def tail: RList[T]
+
+  def isEmpty: Boolean
+
+  def ::[S >: T](elem: S): RList[S] = new ::(elem, this)
+
+  def apply(index: Int): T
+}
+
+case object RNil extends RList[Nothing] {
+  override def head: Nothing = throw new NoSuchElementException()
+
+  override def tail: RList[Nothing] = throw new NoSuchElementException()
+
+  override def isEmpty: Boolean = true
+
+  override def toString: String = "[]"
+
+  override def apply(index: Int): Nothing = throw new NoSuchElementException()
+}
+
+case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T] {
+  override def isEmpty: Boolean = false
+
+  override def toString: String = {
+    @tailrec
+    def toStringHelper(remaining: RList[T], result: String): String = {
+      if (remaining.isEmpty) result
+      else if (remaining.tail.isEmpty) s"$result${remaining.head}"
+      else toStringHelper(remaining.tail, s"$result${remaining.head}, ")
+    }
+
+    s"[${toStringHelper(this, "")}]"
+  }
+
+  // Complexity: O(min(N, index)
+  override def apply(index: Int): T = {
+    @tailrec
+    def applyHelper(remaining: RList[T], currentIndex: Int): T = {
+      if (currentIndex == index) remaining.head
+      else applyHelper(remaining.tail, currentIndex + 1)
+    }
+
+    if (index < 0) throw new NoSuchElementException()
+    else applyHelper(this, 0)
+  }
+}
+
+object ListProblems extends App {
+  val list = 1 :: 2 :: 3 :: 9 :: 8 :: 7 :: RNil
+
+  println(list) // [1, 2, 3, 9, 8, 7]
+
+  println(list(0)) // 1
+  println(list(1)) // 2
+  println(list(2)) // 3
+}
