20a7bc2398af9f86639aa48bcaf82c0e55c7f77b
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-07-13 13:42:10 +0530

    Check if number is ugly

--- src/main/scala/com/rockthejvm/numbers/NumberProblems.scala
+++ src/main/scala/com/rockthejvm/numbers/NumberProblems.scala
@@ -126,6 +126,22 @@ object NumberOps {
         -reverseHelper(-n, 0)
     }
 
+    /*
+      Ugly numbers are those numbers whose prime factors are 2, 3 or 5 only
+     */
+    def isUgly: Boolean = {
+      if (n == 1)
+        true
+      else if (n % 2 == 0)
+        (n / 2).isUgly
+      else if (n % 3 == 0)
+        (n / 3).isUgly
+      else if (n % 5 == 0)
+        (n / 5).isUgly
+      else
+        false
+    }
+
   }
 
 }
@@ -165,4 +181,14 @@ object NumberProblems extends App {
   println(-504.reverse) // -405
   println(-540.reverse) // -45
   println(-53678534.reverse) // -43587635
+
+  println(1.isUgly) // true
+  println(2.isUgly) // true
+  println(3.isUgly) // true
+  println(5.isUgly) // true
+  println(6.isUgly) // true
+  println(25.isUgly) // true
+  println(100.isUgly) // true
+  println(14.isUgly) // false
+  println(39.isUgly) // false
 }
