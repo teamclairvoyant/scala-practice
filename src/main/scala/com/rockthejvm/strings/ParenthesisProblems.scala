745b7bb69405c6abf925845702dd1044e531095e
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-07-25 12:10:17 +0530

    Check if string has valid parenthesis

---
+++ src/main/scala/com/rockthejvm/strings/ParenthesisProblems.scala
@@ -0,0 +1,27 @@
+package com.rockthejvm.strings
+
+import scala.annotation.tailrec
+
+object ParenthesisProblems extends App {
+
+  def hasValidParenthesis(str: String): Boolean = {
+    @tailrec
+    def hasValidParenthesisHelper(remaining: String, openParenthesisCount: Int): Boolean = {
+      if (remaining.isEmpty)
+        openParenthesisCount == 0
+      else if (remaining.head == ')' && openParenthesisCount == 0)
+        false
+      else if (remaining.head == ')')
+        hasValidParenthesisHelper(remaining.tail, openParenthesisCount - 1)
+      else
+        hasValidParenthesisHelper(remaining.tail, openParenthesisCount + 1)
+    }
+
+    hasValidParenthesisHelper(str, 0)
+  }
+
+  println(hasValidParenthesis("()")) // true
+  println(hasValidParenthesis("()()")) // true
+  println(hasValidParenthesis("(())")) // true
+  println(hasValidParenthesis(")(")) // false
+}
