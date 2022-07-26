6105243b165e9e803b4b6e2d0641ad168c7feff9
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-07-26 12:35:28 +0530

    Generate valid parenthesis

--- src/main/scala/com/rockthejvm/strings/ParenthesisProblems.scala
+++ src/main/scala/com/rockthejvm/strings/ParenthesisProblems.scala
@@ -24,5 +24,35 @@ object ParenthesisProblems extends App {
   println(hasValidParenthesis("()()")) // true
   println(hasValidParenthesis("(())")) // true
   println(hasValidParenthesis(")(")) // false
-  println(hasValidParenthesis(")()")) // false
+
+  def generateAllValidParenthesis(n: Int): List[String] = {
+    @tailrec
+    def generateAllValidParenthesisHelper(nRemaining: Int, currentStrings: Set[String]): Set[String] = {
+      if (nRemaining == 0)
+        currentStrings
+      else {
+        val newStrings =
+          for {
+            string <- currentStrings
+            index <- 0 until string.length
+          } yield {
+            val (before, after) = string.splitAt(index)
+            s"$before()$after"
+          }
+
+        generateAllValidParenthesisHelper(nRemaining - 1, newStrings)
+      }
+    }
+
+    assert(n >= 0)
+
+    if (n == 0)
+      List.empty
+    else
+      generateAllValidParenthesisHelper(n - 1, Set("()")).toList
+  }
+
+  println(generateAllValidParenthesis(1)) // List(())
+  println(generateAllValidParenthesis(2)) // List(()(), (()))
+  println(generateAllValidParenthesis(3)) // List((()()), ((())), ()()(), (())(), ()(()))
 }
